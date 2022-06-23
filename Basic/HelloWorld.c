cb2fa501573293dde98b644a8c7dae236dd550f1
GopalChandora <gopalchandora38@gmail.com>
2022-06-23 15:07:18 +0530

    Create HelloWorld.c

---
+++ Basic/HelloWorld.c
@@ -0,0 +1,8 @@
+// To print our first Output Hello World on Terminal.
+#include<stdio.h>
+
+int main()
+{
+printf("Hello World"); //printf - To print output on terminal, Terminated with semi-colon. 
+return 0;
+}
