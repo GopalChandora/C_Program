53bc7fe2f9b3876c785df1a9288d2059d430a5ee
GopalChandora <gopalchandora38@gmail.com>
2022-07-07 16:07:55 +0530

    Create WhileLoop Natural_Number.c

---
+++ Loop/WhileLoop/Natural_Number.c
@@ -0,0 +1,53 @@
+// To print Natural Number.
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int i;
+    printf("Natural Number are: \n");
+    while (i<=10)
+    {
+        printf("%d\n",i);
+        i++;
+    }
+    
+    /*i=10;
+    printf("Natural Number in Reverse Order are: \n");
+    while (i>=1)
+    {
+        printf("%d\n",i);
+        i--;
+    }*/    
+}
+/*{
+    int num,i;
+    printf("T print Natural Number in Ascending Order ");
+    printf("\nEnter Starting Value : ");
+    scanf("%d",&num);
+    printf("\nEnter Ending Value : ");
+    scanf("%d",&i);
+    printf("\nNatural Number from %d to %d", num,i);
+    while (num<=i)
+    {
+        printf("\n%d",num);
+        num++;
+    }    
+}*/
+
+
+/*{
+    int num,i;
+    printf("To print Natural Number in reverse Order :");
+    printf("\nEnter Starting Value : ");
+    scanf("%d",&num);
+    printf("Enter Ending Value : ");
+    scanf("%d",&i);
+    printf("Natural Number from %d to %d are",num,i);
+    while (num>=i)
+    {
+        printf("\n%d",num);
+        num--;
+    }
+}*/
